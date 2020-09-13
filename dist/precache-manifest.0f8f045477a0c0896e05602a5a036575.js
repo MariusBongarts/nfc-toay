d78237b01d8e44a72ac3db50e70f7aa42de14131
Marius Bongarts <mariusbongarts@web.de>
2020-09-13 13:29:00 +0200

    Push dist folder

---
+++ dist/precache-manifest.0f8f045477a0c0896e05602a5a036575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1cd537fd802839428b",
+    "url": "/css/app.dcd5b955.css"
+  },
+  {
+    "revision": "5b2e28a4e2d96339da4616946543b692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "714b510c1e228d734c02",
+    "url": "/js/about.71666a23.js"
+  },
+  {
+    "revision": "aa1cd537fd802839428b",
+    "url": "/js/app.a759ba6a.js"
+  },
+  {
+    "revision": "3798b81c2acc6ca1d30a",
+    "url": "/js/chunk-vendors.08eecfb0.js"
+  },
+  {
+    "revision": "2d9167731623c8980ef70cba74b28d2f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
